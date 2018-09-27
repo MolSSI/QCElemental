976f92aa2af7d741af9098ae061350d9b4518c29
Daniel Smith <malorian@me.com>
2018-09-27 16:03:01 -0400

    Units: Tweaks ureg

--- qcelemental/tests/test_units.py
+++ qcelemental/tests/test_units.py
@@ -6,12 +6,37 @@ import os
 import qcelemental
 
 
-@pytest.mark.parametrize("inp,expected,tol", [
-    ("kcal/mol", 627.509474, 1.e-6),
-    ("J/mol", 2625.499638*1000, 1.e-6),
-    ("kJ/mol", 2625.499638, 1.e-6),
-    ("eV", 27.21138602, 1e-6),
+# 2014 CODATA values
+@pytest.mark.parametrize("from_unit, to_unit, expected", [
+    ("hartree", "kcal/mol", "627.509474"),
+    ("hartree", "J/mol", "2625499.638"),
+    ("hartree", "kJ/mol", "2625.499638"),
+    ("hartree", "eV", "27.21138602"),
+    ("bohr", "angstrom", "0.52917721067"),
+    ("bohr", "nanometer", "5.2917721067e-2"),
+    ("bohr", "cm", "5.2917721067e-9"),
+    ("amu", "kg", "1.660539040e-27"),
+    ("amu", "g", "1.660539040e-24"),
+    ("hartree", "wavenumber", "2.194746313702e5"),
+
+    # Precision is truncated due to uncertainty issues
+    # ("hartree", "Hz", "6.579683920711e15"),
+    ("hartree", "Hz", "6.5796839208e15"),
 ])
-def test_hartree_conversion(inp, expected, tol):
-    assert qcelemental.units.conversion_factor("hartree", inp) == pytest.approx(expected, tol)
-    assert qcelemental.units.conversion_factor(inp, "hartree") == pytest.approx(1.0 / expected, tol)
+def test_unit_conversion(from_unit, to_unit, expected):
+
+    # Build values and quantize to the tolerance
+    expected = Decimal(expected)
+    from_to_value = Decimal(qcelemental.units.conversion_factor(from_unit, to_unit)).quantize(expected)
+    assert from_to_value == expected
+
+    # Build inverse
+    inv_expected = 1 / Decimal(expected)
+    to_from_value = Decimal(qcelemental.units.conversion_factor(to_unit, from_unit))
+
+    # Expected to a relative tolerance as the number of digits plus two for rounding
+    # Using float comparisons as we are taking an (1 / float) inverse in the conversion code
+    rel_tol = float("10e-{}".format(len(expected.as_tuple().digits) + 2))
+    assert pytest.approx(float(inv_expected), rel_tol) == float(to_from_value)
+
+
--- qcelemental/units/ureg.py
+++ qcelemental/units/ureg.py
@@ -15,22 +15,48 @@ ureg = pint.UnitRegistry()
 Quantity = ureg.Quantity
 
 # Explicitly update relevant 2014 codata
+
+# Definitions
 ureg.define("avogadro_constant = {} / mol = N_A".format(phys_const["avogadro constant"]["value"]))
-ureg.define("hartree = {} * joule = E_h = hartree_energy".format(phys_const["hartree energy"]["value"]))
-ureg.define("bohr = {} * meter = bohr_radius".format(phys_const["bohr radius"]["value"]))
 ureg.define("boltzmann_constant = {} * joule / kelvin".format(phys_const["boltzmann constant"]["value"]))
+ureg.define("speed_of_light = {} * meter / second".format(phys_const["speed of light in vacuum"]["value"]))
+ureg.define("hartree_inverse_meter = {} / hartree / m".format(phys_const["hartree-inverse meter relationship"]["value"]))
+
+# Energy
+ureg.define("hartree = {} * joule = E_h = hartree_energy".format(phys_const["hartree energy"]["value"]))
+ureg.define("electron_volt = {} * J = eV".format(phys_const["electron volt-joule relationship"]["value"]))
+
+# Constants
+ureg.define("electron_mass = {} * kg".format(phys_const["electron mass"]["value"]))
 ureg.define("elementary_charge = {} * coulomb = e".format(phys_const["elementary charge"]["value"]))
+ureg.define("plancks_constant = {} * joule * s".format(phys_const["planck constant"]["value"]))
+
+# Distance
+ureg.define("bohr = {} * meter = bohr_radius".format(phys_const["bohr radius"]["value"]))
+ureg.define("wavenumber = 1 / centimeter")
+
+# Masses
+ureg.define(
+    "atomic_mass_unit = {} * kilogram = u = amu = dalton = Da".format(phys_const["atomic mass constant"]["value"]))
 
 # Add contexts
-c = pint.Context("quantum_chemistry")
+c1 = pint.Context("quantum_chemistry")
 
-# Allows hartree <-> energy / mol
-energy = pint.util.UnitsContainer({'[energy]': 1})
-energy_substance = pint.util.UnitsContainer({'[energy]': 1, '[substance]': -1})
-c.add_transformation(energy, energy_substance, lambda ureg, val : val * ureg.N_A)
-c.add_transformation(energy_substance, energy, lambda ureg, val : val / ureg.N_A)
+# Allows hartree <-> frequency
+c1 = pint.Context("energy_frequency")
+c1.add_transformation("[energy]", "[frequency]", lambda ureg, val: val / ureg.plancks_constant)
+c1.add_transformation("[frequency]", "[energy]", lambda ureg, val: val * ureg.plancks_constant)
 
+# Allows hartree <-> length
+c2 = pint.Context("energy_length")
+c2.add_transformation("[energy]", "1 / [length]", lambda ureg, val: val * ureg.hartree_inverse_meter)
+c2.add_transformation("1 / [length]", "[energy]", lambda ureg, val: val / ureg.hartree_inverse_meter)
+
+# Allows energy <-> energy / mol
+c3 = pint.Context("substance_relation")
+c3.add_transformation("[energy]", "[energy] / [substance]", lambda ureg, val: val * ureg.N_A)
+c3.add_transformation("[energy] / [substance]", "[energy]", lambda ureg, val: val / ureg.N_A)
 
 # Add the context
-ureg.add_context(c)
-ureg.enable_contexts("quantum_chemistry")
+ureg.enable_contexts(c1, c2, c3)
+
