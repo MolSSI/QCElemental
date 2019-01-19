aff488ea5e857df640d78694be49dc36389394d7
Lori A. Burns <lori.burns@gmail.com>
2019-01-19 15:56:42 -0500

    hide private fn in testing

--- qcelemental/tests/utils.py
+++ qcelemental/tests/utils.py
@@ -3,7 +3,7 @@ import sys
 import utils_compare
 
 
-def true_false_decorator(compare_fn, *args, **kwargs):
+def _true_false_decorator(compare_fn, *args, **kwargs):
     """Turns `compare_fn` that returns `None` on success and raises
     `_TestComparisonError` on failure into a function that returns
     True/False, suitable for assertions in pytest.
@@ -21,13 +21,13 @@ def true_false_decorator(compare_fn, *args, **kwargs):
     return true_false_wrapper
 
 
-compare_values = true_false_decorator(utils_compare.compare_values)
-compare_strings = true_false_decorator(utils_compare.compare_strings)
-compare_integers = true_false_decorator(utils_compare.compare_integers)
-#compare_matrices = true_false_decorator(utils_compare.compare_matrices)
-#compare_arrays = true_false_decorator(utils_compare.compare_arrays)
-compare_dicts = true_false_decorator(utils_compare.compare_dicts)
-compare_molrecs = true_false_decorator(utils_compare.compare_molrecs)
+compare_values = _true_false_decorator(utils_compare.compare_values)
+compare_strings = _true_false_decorator(utils_compare.compare_strings)
+compare_integers = _true_false_decorator(utils_compare.compare_integers)
+#compare_matrices = _true_false_decorator(utils_compare.compare_matrices)
+#compare_arrays = _true_false_decorator(utils_compare.compare_arrays)
+compare_dicts = _true_false_decorator(utils_compare.compare_dicts)
+compare_molrecs = _true_false_decorator(utils_compare.compare_molrecs)
 
 
 def tnm():
