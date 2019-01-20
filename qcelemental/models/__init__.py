ea1549f9c2ddeff4d2664750a83c3b369e053b42
Lori A. Burns <lori.burns@gmail.com>
2019-01-20 04:03:10 -0500

    Update __init__.py

--- qcelemental/models/__init__.py
+++ qcelemental/models/__init__.py
@@ -1,1 +1,6 @@
+try:
+    import pydantic
+except ImportError:
+    raise ImportError("""Python module pydantic not found. Solve by installing it: `conda install pydantic -c conda-forge` or `pip install pydantic`""")
+
 from .objects import Molecule
