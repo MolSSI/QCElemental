21d0c7ced8b853021e556b660c24bb1dc01ebceb
Daniel Smith <malorian@me.com>
2019-02-08 10:30:40 -0500

    Dep: Fixes small deprecation warning on pint import

--- qcelemental/physical_constants/__init__.py
+++ qcelemental/physical_constants/__init__.py
@@ -3,9 +3,10 @@ Units handlers
 """
 
 # Ensure pint exists
-import importlib
-pint_loader = importlib.find_loader('pint')
+from importlib.util import find_spec
+pint_loader = find_spec('pint')
 if pint_loader is None:
     raise ImportError("""Python module pint not found. Solve by installing it: `conda install pint -c conda-forge` or `pip install pint`""")
+del find_spec
 
 from .context import constants, PhysicalConstantsContext
