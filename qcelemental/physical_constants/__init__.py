2a56b834cfacedf99f5a6db1aa7857889bac17b2
Daniel Smith <malorian@me.com>
2019-02-08 10:36:13 -0500

    Pint: Correctly uses new find_spec

--- qcelemental/physical_constants/__init__.py
+++ qcelemental/physical_constants/__init__.py
@@ -4,9 +4,9 @@ Units handlers
 
 # Ensure pint exists
 from importlib.util import find_spec
-pint_loader = find_spec('pint')
-if pint_loader is None:
-    raise ImportError("""Python module pint not found. Solve by installing it: `conda install pint -c conda-forge` or `pip install pint`""")
-del find_spec
+spec = find_spec('pint')
+if spec is None:
+    raise ModuleNotFoundError("""Python module pint not found. Solve by installing it: `conda install pint -c conda-forge` or `pip install pint`""")
+del spec, find_spec
 
 from .context import constants, PhysicalConstantsContext
