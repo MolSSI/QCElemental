ecd250ad245cdc879a545af17e23ae4cf98f447e
Lori A. Burns <lori.burns@gmail.com>
2019-04-23 17:38:06 -0400

    adjust import positioning

--- qcelemental/__init__.py
+++ qcelemental/__init__.py
@@ -5,7 +5,6 @@ Main init for QCElemental
 from .datum import Datum
 from .exceptions import (NotAnElementError, ValidationError, MoleculeFormatError, ChoicesError, DataUnavailableError)
 from .testing import (compare, compare_values)
-from .importing import (parse_version, safe_version, which, which_import)
 from . import molparse
 from . import models
 
--- qcelemental/physical_constants/__init__.py
+++ qcelemental/physical_constants/__init__.py
@@ -2,11 +2,4 @@
 Units handlers
 """
 
-# Ensure pint exists
-from ..importing import which_import
-if not which_import('pint', return_bool=True):
-    raise ModuleNotFoundError(
-        """Python module "pint" not found. Solve by installing it: `conda install pint -c conda-forge` or `pip install pint`"""
-    )
-
 from .context import constants, PhysicalConstantsContext
