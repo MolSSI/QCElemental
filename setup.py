ed68cf693fdf1f6bf9be87f9ed4f5fd5e99a42d2
Andrew-AbiMansour <andrew.gaam@gmail.com>
2021-03-05 13:45:29 -0500

    add yaml to extras_require & format code

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ short_description = "QCElemental is a resource module for quantum chemistry cont
 "constants and periodic table data from NIST and molecule handlers."
 
 # from https://github.com/pytest-dev/pytest-runner#conditional-requirement
-needs_pytest = {'pytest', 'test', 'ptr'}.intersection(sys.argv)
-pytest_runner = ['pytest-runner'] if needs_pytest else []
+needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
 
 try:
     with open("README.md", "r") as handle:
@@ -18,59 +18,64 @@ except FileNotFoundError:
 
 if __name__ == "__main__":
     setuptools.setup(
-        name='qcelemental',
-        description='Essentials for Quantum Chemistry.',
-        author='The QCArchive Development Team',
-        author_email='qcarchive@molssi.org',
+        name="qcelemental",
+        description="Essentials for Quantum Chemistry.",
+        author="The QCArchive Development Team",
+        author_email="qcarchive@molssi.org",
         url="https://github.com/MolSSI/QCElemental",
-        license='BSD-3C',
+        license="BSD-3C",
         version=versioneer.get_version(),
         cmdclass=versioneer.get_cmdclass(),
-        packages=setuptools.find_packages(exclude=['*checkup*']),
+        packages=setuptools.find_packages(exclude=["*checkup*"]),
         include_package_data=True,
-        package_data={'': [os.path.join('qcelemental', 'data', '*.json')]},
+        package_data={"": [os.path.join("qcelemental", "data", "*.json")]},
         setup_requires=[] + pytest_runner,
-        python_requires='>=3.6',
+        python_requires=">=3.6",
         install_requires=["numpy >= 1.12.0", "pint >= 0.10.0", "pydantic >=1.5.0,!=1.6.0"],
         extras_require={
-            'docs': [
-                'numpydoc',
-                'sphinx',  # autodoc was broken in 1.3.1
-                'sphinxcontrib-napoleon',
-                'sphinx_rtd_theme',
+            "docs": [
+                "numpydoc",
+                "sphinx",  # autodoc was broken in 1.3.1
+                "sphinxcontrib-napoleon",
+                "sphinx_rtd_theme",
             ],
-            'tests': [
-                'pytest >= 4.0.0',
-                'pytest-cov',
+            "tests": [
+                "pytest >= 4.0.0",
+                "pytest-cov",
                 # 'jsonschema',  # needed for speciality `pytest --validate`
             ],
-            'align': [
-                'networkx>=2.4.0',
+            "align": [
+                "networkx>=2.4.0",
             ],
-            'viz': [
-                'nglview',
+            "viz": [
+                "nglview",
             ],
-            'lint': [
-                'autoflake',
-                'black',
-                'isort',
+            "lint": [
+                "autoflake",
+                "black",
+                "isort",
+            ],
+            "yaml": [
+                "pyyaml",  # supports YAML 1.1
+                # 'ruamel.yaml', # supports YAML 1.2
             ],
         },
         tests_require=[
-            'pytest >= 4.0.0',
-            'pytest-cov',
+            "pytest >= 4.0.0",
+            "pytest-cov",
             # 'jsonschema',  # needed for speciality `pytest --validate`
         ],
         classifiers=[
-            'Development Status :: 4 - Beta',
-            'Intended Audience :: Science/Research',
-            'Programming Language :: Python :: 3 :: Only',
-            'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.6',
-            'Programming Language :: Python :: 3.7',
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Science/Research",
+            "Programming Language :: Python :: 3 :: Only",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: 3.8",
+            "Programming Language :: Python :: 3.9",
         ],
         zip_safe=False,
         long_description=long_description,
-        long_description_content_type="text/markdown")
+        long_description_content_type="text/markdown",
+    )
