b32b2a418b4978f610d23032a7dcde68da2fceea
loriab <loriab@users.noreply.github.com>
2021-09-15 03:05:15 +0000

    Deploying to gh-pages from @ MolSSI/QCElemental@f401b490ee456e8b20105db472cdc7e87c8b6366 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g6e06fb7',
+    VERSION: '0+untagged.1.gf401b49',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
