044d165fa4b289b71b7dcc6fb2fdef5d1eb0275e
WardLT <WardLT@users.noreply.github.com>
2022-03-24 20:40:02 +0000

    Deploying to gh-pages from @ MolSSI/QCElemental@d04a380fd80e6634648ae7fe4c4305dd1f4b5070 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.gcabec4a',
+    VERSION: '0+untagged.1.gd04a380',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
