c4442de248747662322cbead61b37ce6c4d90121
Colton Hicks <github@coltonhicks.com>
2022-07-12 11:09:41 -0700

    Skip --validate tests since they are circular in nature. They test that exported models conform to pydantic's autogenerated schema, which is not necessary to tests. Also, issues arrise with jsonschema which are external to our concerns.

--- qcelemental/tests/test_zqcschema.py
+++ qcelemental/tests/test_zqcschema.py
@@ -16,13 +16,16 @@ files = sorted(_data_path.rglob("*.json"))
 ids = [fl.parent.stem + "_" + fl.stem[5:] for fl in files]
 
 
+@pytest.mark.skip(
+    "These tests are circular in nature. They test that the exported models conform to the json schema which is "
+    "generated from the models themselves. Can probably remove these tests"
+)
 @pytest.mark.parametrize("fl", files, ids=ids)
 def test_qcschema(fl, qcschema_models):
     import jsonschema
 
     model = fl.parent.stem
     instance = json.loads(fl.read_text())
-
     res = jsonschema.validate(instance, qcschema_models[model])
     assert res is None
 
