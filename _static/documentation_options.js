581eb7062ff6a1c3f1be0011864f93e143e124bf
coltonbh <github@coltonhicks.com>
2023-03-27 17:03:36 +0000

    Deploying to gh-pages from @ MolSSI/QCElemental@86b4700dacc6322998d8580b352f1d27cde33962 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0+untagged.1.g292350f',
+    VERSION: 'v0+untagged.1.g86b4700',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
