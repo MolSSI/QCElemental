8b27912caaf742090818dbc7b3304a0520c154f9
Benjamin Pritchard <ben@bennyp.org>
2023-09-29 09:56:17 -0400

    Add eigenvalues_and_occupations to wavefunction protocols

--- qcelemental/tests/test_model_results.py
+++ qcelemental/tests/test_model_results.py
@@ -307,6 +307,18 @@ def test_wavefunction_return_result_pointer(wavefunction_data_fixture):
             ["orbitals_a", "eigenvalues_a"],
         ),
         ("return_results", True, ["orbitals_a", "fock_a", "fock_b"], ["orbitals_a", "fock_a"]),
+        (
+            "eigenvalues_and_occupations",
+            True,
+            ["orbitals_a", "orbitals_b", "occupations_a", "occupations_b", "eigenvalues_a", "eigenvalues_b"],
+            ["occupations_a", "eigenvalues_a"],
+        ),
+        (
+            "eigenvalues_and_occupations",
+            False,
+            ["orbitals_a", "orbitals_b", "occupations_a", "occupations_b", "eigenvalues_a", "eigenvalues_b"],
+            ["occupations_a", "occupations_b", "eigenvalues_a", "eigenvalues_b"],
+        ),
     ],
 )
 def test_wavefunction_protocols(protocol, restricted, provided, expected, wavefunction_data_fixture, request):
